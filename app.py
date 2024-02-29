d868577d7969119ca993b84ee26b86a6eda07450
danielbonadiez <151293277+danielbonadiez@users.noreply.github.com>
2024-02-29 09:40:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+# Importar el módulo Flask
 from flask import Flask, jsonify
 
+# Crear una instancia de la aplicación Flask
 app = Flask(__name__)
 
+# Función de vista para la ruta '/personas'
 @app.route('/personas', methods=['GET'])
 def get_personas():
+    
+    # Lista de nombres de personas
     personas = [
         {'nombre': 'Juan'},
         {'nombre': 'María'},
@@ -11,6 +16,7 @@ def get_personas():
         {'nombre': 'kiara'},
         {'nombre': 'Luis'},
     ]
+    # Convertir la lista en formato JSON
     return jsonify(personas)
 
 if __name__ == '__main__':
